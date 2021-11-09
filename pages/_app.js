5d75065e049d020bf5bb570cdac4ca277ec806e7
assassin21r <assassinlmao21@gmail.com>
2021-11-09 16:22:28 +0530

    basic nav and section added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Home from './home'
+import Layout from './layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Home />
+    </Layout>
+  )
 }
 
 export default MyApp
